17d8f177eb4116d9f002ed1660c39ed83011fb3e
Jonathan Stoppani <jonathan@stoppani.name>
2011-08-29 13:07:40 -0400

    Implemented complete/incomplete filtering

--- uga/registration/admin.py
+++ uga/registration/admin.py
@@ -1,6 +1,31 @@
 from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
+
 from uga.registration import models
 
+
+class IncompleteListFilter(admin.SimpleListFilter):
+    title = _('completion')
+
+    parameter_name = 'status'
+    
+    def lookups(self, request, model_admin):
+        return (
+            ('complete', _('Complete')),
+            ('incomplete', _('Incomplete')),
+        )
+
+    def queryset(self, request, queryset):
+        query = queryset.model.objects._get_completion_query()
+
+        if self.value() == 'incomplete':
+            query = ~query
+
+        return queryset.filter(query)
+
+
+
 class MemberAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('first_name', 'last_name', 'email', 'is_complete')
+    list_filter = (IncompleteListFilter,)
 admin.site.register(models.Member, MemberAdmin)
--- uga/registration/models.py
+++ uga/registration/models.py
@@ -1,10 +1,30 @@
 from django.db import models
+from django.db.models import Q
+
+
+class IncompleteMemberManager(models.Manager):
+
+    def _get_completion_query(self):
+        return ~Q(last_name='') & ~Q(first_name='')
+
+    def incomplete(self):
+        return self.filter(self._get_completion_query())
+
+    def complete(self):
+        return self.filter(~self._get_completion_query())
 
 
 class Member(models.Model):
     """
     A single member of the association.
     """
+    objects = IncompleteMemberManager()
+
     email = models.EmailField(blank=False, unique=True)
     first_name = models.CharField(max_length=255, blank=True, null=True)
     last_name = models.CharField(max_length=255, blank=True, null=True)
+
+    def is_complete(self):
+        return bool(self.first_name and self.last_name)
+    is_complete.boolean = True
+
