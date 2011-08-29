8e285636b4f9db976e2598ff22664693d7bb7bbe
Jonathan Stoppani <jonathan@stoppani.name>
2011-08-29 13:07:27 -0400

    Updated database settings to multidb format

--- settings/common.py
+++ settings/common.py
@@ -18,12 +18,16 @@ IGNORABLE_404_ENDS = ['favicon.ico','favicon.png',]
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = 'sqlite3'           # '', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = os.path.join(PROJECT_BASE, 'assets', 'database.sqlite')             # Or path to database file if using sqlite3.
-DATABASE_USER = ''             # Not used with sqlite3.
-DATABASE_PASSWORD = ''         # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+DATABASES = {
+    'default': {
+        'ENGINE':'sqlite3',
+        'NAME': os.path.join(PROJECT_BASE, 'assets', 'database.sqlite'),
+        'USER': '',             # Not used with sqlite3.
+        'PASSWORD': '',         # Not used with sqlite3.
+        'HOST': '',             # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',             # Set to empty string for default. Not used with sqlite3.
+    }
+}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
