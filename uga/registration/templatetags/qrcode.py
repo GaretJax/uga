f51e8d911b63b69fbc1d961419b3ebacc2bc95eb
Jonathan Stoppani <jonathan@stoppani.name>
2012-08-05 12:18:59 +0200

    Added template tag to generate QR code image url for google apis from a string

---
+++ uga/registration/templatetags/qrcode.py
@@ -0,0 +1,34 @@
+"""
+Based on http://djangosnippets.org/snippets/1494/
+"""
+
+import urllib
+from django import template
+from django.template.defaultfilters import stringfilter
+from django.utils.html import conditional_escape
+from django.utils.safestring import mark_safe
+
+register = template.Library()
+
+@register.filter
+@stringfilter
+def qrcode(value, size='150x150'):
+    """
+    Generate QR Code image from a string with the Google charts API
+
+    http://code.google.com/intl/fr-FR/apis/chart/types.html#qrcodes
+
+    Exemple usage --
+    {{ my_string|qrcode:'150x150' }}
+
+    <img src="http://chart.apis.google.com/chart?chs=150x150&amp;cht=qr&amp;chl=my_string&amp;choe=UTF-8" alt="my alt" />
+    """
+
+    width, height = size.split('x')
+    width, height = int(width), int(height)
+    base = 'http://chart.apis.google.com/chart'
+    query = urllib.urlencode({'chs':'{}x{}'.format(width, height), 'cht':'qr', 'chl':value, 'choe':'UTF-8', 'chld': 'h|0'})
+    url = conditional_escape('{}?{}'.format(base, query))
+    alt = conditional_escape(value)
+
+    return mark_safe(u'<img class="qrcode" src="{}" width="{}" height="{}" alt="{}" />'.format(url, alt, width, height))
